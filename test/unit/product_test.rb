1ae4719361ad97c6a2a98d036905efa6f921d742
宮部 泰治 <miyabetaiji@miyabe-no-MacBook-Air.local>
2011-12-25 16:11:29 +0900

    unit tests

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attribute must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price msut be positive" do
+    product = Product.new(title:        "My book tiels",
+                          description:  "yyy",
+                          image_url:    "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+      product.errors[:price].join('; ')
+    
+    product.price = 0
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+      product.errors[:price].join('; ')
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title:        "My book tiels",
+                description:  "yyy",
+                price:        1,
+                image_url:    image_url)
+  end
+
+  test "image_url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c./x/y/x.fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gid.more }
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shoudn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shoudn't be valid"
+    end
+
+  end
 end
