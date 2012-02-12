7328c035f7c9da22662a71ee5ee59ede29ebad10
a1153tm <a1153tm@aiit.ac.jp>
2012-02-13 01:09:12 +0900

    add cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensuure_not_referenced_by_any_line_item
+
+  private
+    def ensuure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'leave items')
+        return false
+      end
+    end
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
